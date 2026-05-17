3b679067a7fe07abe65d060f13457a58ae6f26bf
SeniorMoses <mosessenior11@gmail.com>
2026-05-17 03:09:44 +0000

    Update News.py

--- News.py
+++ News.py
@@ -89,8 +89,8 @@ def get_user(token:str =Depends(Oauth2_scheme)):
         detail='invalid token'
         ) 
         
-        
-        
+cursor.execute(
+    'INSERT INTO users(role, username, email, password) VALUES(?,?,?,?), ['admin', 'senior', 'mosessenior11@gmail.com', 'developer111']
 @app.get('/')
 def home():
     return {'message': 'fastapi server is running🚀'}
